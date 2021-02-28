1034f5481f962593e1374effbc04ef8cf8446812
Jenny Kim <kimj1905@gmail.com>
2021-02-28 17:06:22 -0500

    commit tree 0f18d59f6dc88b80cd8e9de387d5284b40210cfe

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+    #while array.size == 2
+        #array.join(" and ")
+    #end
+    if array.size == 2
+        array.join(" and ")
+    elsif array.size > 2    
+        array[0..-2].join(", ") << ", and " << array[-1]
+        #(" , ") << array[-1].join("and ")
+        #not_final = array.slice(0, -1)
+        #final = array[-1]
+        #not_final_two = not_final.join(" , ")
+        #final_two = final.join("and ")
+        #"#{not_final_two} #{final_two}  "
+    else
+        array.join
+    end
 end
