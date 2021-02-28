9637904fb7cddf7e2864dd78244e0b054744636f
Jenny Kim <kimj1905@gmail.com>
2021-02-28 17:06:41 -0500

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    #while array.size == 2
+        #array.join(" and ")
+    #end
+    if array.size == 2
+        array.join(" and ")
+    elsif array.size > 2    
+        array[0..-2].join(", ") << ", and " << array[-1]
+    else
+        array.join
+    end
 end
